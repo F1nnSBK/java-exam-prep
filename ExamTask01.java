2b3968c8e2437a0007e2ff53e3246a422657d410
Finn <fhertsch235@gmail.com>
2025-03-07 21:38:46 +0100

    test

---
+++ ExamTask01.java
@@ -0,0 +1,9 @@
+public class ExamTask01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Person finn = new Person(36348.43);
+
+        System.out.println("Finn hat " + finn.budget + "€");
+    }
+}
